736b717d4bfc37630737e444d42f52498f39137d
kbjung <kbjoong@daum.net>
2022-01-05 16:36:43 +0900

    Create StringApp.java

---
+++ Data_and_operation/src/StringApp.java
@@ -0,0 +1,17 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// Character vs String
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+		System.out.println("H"); // String
+
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\""); // Hello "World"
+	}
+
+}
